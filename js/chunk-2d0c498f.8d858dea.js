8da72766f4295a2fa98303cc6a10205efe667fa7
ldrovira <luisdanielrovira8@gmail.com>
2018-10-11 15:01:21 -0400

    deploy

---
+++ js/chunk-2d0c498f.8d858dea.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c498f"],{"3c09":function(e,t,n){"use strict";n.r(t);var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},i=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an Whitepapar page")])])}],s=n("2877"),c={},r=Object(s["a"])(c,a,i,!1,null,null,null);r.options.__file="Whitepaper.vue";t["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0c498f.8d858dea.js.map
